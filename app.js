b6722e397161b04040fba6ab583307d3e91b04c6
mrcyme <simeonmcl@gmail.com>
2024-10-15 09:07:37 +0200

    removed authentififation

--- app.js
+++ app.js
@@ -6,23 +6,6 @@ let pageHistory = ["home"]; // Start with the home page
 let demoId = parseInt(window.location.pathname.split("/")[1]);
 //if now demo id is supplied in the url then return to demo 3 (simplex)
 
-// Path to the key.json file
-const keyFilePath = "keys.json";
-
-// Function to read and parse the key.json file
-async function getAccessToken() {
-    try {
-        const response = await fetch(keyFilePath);
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        const keyData = await response.json();
-        return keyData.directus_access_token;
-    } catch (error) {
-        console.error("Error reading or parsing key.json file: ", error);
-        return null;
-    }
-}
 
 
 demoId = Number.isInteger(demoId) ? demoId : 58;
@@ -30,17 +13,8 @@ demoId = Number.isInteger(demoId) ? demoId : 58;
 let demoContent;
 
 async function loadDemoFromDirectus() {
-  const access_token = await getAccessToken();
-    if (!access_token) {
-        console.error("Access token not found.");
-        return;
-    }
-
     try {
         const response = await axios.get(`${directusUrl}/items/demos`, {
-          headers: {
-              'Authorization': `Bearer ${access_token}`  // Include token here
-          },
           params: {
               fields: '*.*', // Fetch all fields including all translations
           }});
